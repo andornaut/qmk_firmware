3230c73bf823f837941bd8b96cb16af5b461753a
andornaut <andornaut@users.noreply.github.com>
2025-06-01 11:28:40 -0400

    Update splinter keyboard to v3

--- keyboards/splinter/config.h
+++ keyboards/splinter/config.h
@@ -2,7 +2,6 @@
 
 // Copyright 2024 andornaut (@andornaut)
 // SPDX-License-Identifier: GPL-2.0-or-later
-
 #pragma once
 
 // This method sets the keyboard's handedness by setting a flag in the persistent storage (EEPROM).
@@ -10,8 +9,7 @@
 // Must flash the left side at least once using: qmk flash -bl avrdude-split-left
 #define EE_HANDS
 
-// sets the number of milliseconds to pause after sending a wakeup packet. Disabled by default, you might want to set this to 200 (or higher) if the keyboard does not wake up properly after suspending.
-#define USB_SUSPEND_WAKEUP_DELAY 200
-
-// https://www.reddit.com/r/ErgoMechKeyboards/comments/zg5aiu/qmk_on_blackpill_need_to_unplugreplug_every_time/izgu7a7/
-// #define SPLIT_USB_DETECT
+// https://docs.qmk.fm/platformdev_rp2040#double-tap
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
---
+++ keyboards/splinter/rules.mk
@@ -0,0 +1,2 @@
+# https://docs.qmk.fm/platformdev_rp2040
+MCU = RP2040
